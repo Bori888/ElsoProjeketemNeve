e2b0c0cd84510dcbf20eb1df8fc84e2e1a981cdf
Bori888 <ilyes.bori8@gmail.com>
2024-10-24 14:00:30 +0200

    elso

---
+++ src/csomag/ForrasProgram.java
@@ -0,0 +1,11 @@
+package csomag;
+
+public class ForrasProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Bori!");
+        System.out.print("Hello MEGINT TE utálatos Java!");
+
+    }
+    
+}
